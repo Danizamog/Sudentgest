7b105b4d5454821202d5a3ae1ef6ee84aad4bcda
mar1511 <marciamirandapinto15152002@gmail.com>
2025-10-13 08:00:11 -0400

    Limpieza completa de la rama Marcia2

--- Desktop/Sudentgest-main/Sudentgest-main/Sudentgest-mainforo-mic/front/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'   // 👈 importa el router
-
-const app = createApp(App)
-
-app.use(router)                 // 👈 dile a Vue que use el router
-app.mount('#app')
