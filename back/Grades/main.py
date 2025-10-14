83da5487ab9ce6cadcfc35490e27ee0e921a216a
SebastianOrias <sebastian.orias@ucb.edu.bo>
2025-10-14 09:58:23 -0400

    BackGradesFix

--- back/Grades/main.py
+++ back/Grades/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from routes.note_router import router as note_router
+from routes.grade_router import router as grade_router
 
 app = FastAPI(
-    title="Notes Microservice",
+    title="Grades Microservice",
     description="Microservicio para gestionar las notas de los estudiantes",
     version="1.0.0"
 )
 
 # Registrar el router principal
-app.include_router(note_router)
+app.include_router(grade_router)
 
 @app.get("/")
 async def root():
